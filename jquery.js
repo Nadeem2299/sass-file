f79f27536197225390292c679a17b0e45b0fb7e6
Nadeem2299 <42616384+Nadeem2299@users.noreply.github.com>
2020-04-26 15:53:48 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function(){
+    $("#img4").click(function(){
+$("#imgg").attr("src","img/ramos1.jpg");
+    });
+});
+
+$(document).ready(function(){
+    $("#img3").click(function(){
+$("#imgg").attr("src","img/kimich.jpg");
+    });
+});
+$(document).ready(function(){
+    $("#img1").click(function(){
+$("#imgg").attr("src","img/casilias.jpg");
+    });
+});
+$(document).ready(function(){
+    $("#img2").click(function(){
+$("#imgg").attr("src","img/jordi.jpg");
+    });
+});
+
+$(document).ready(function(){
+    $("#img444").click(function(){
+$("#imgg3").attr("src","img/ronaldo.jpg");
+    });
+});
+
+$(document).ready(function(){
+    $("#img333").click(function(){
+$("#imgg3").attr("src","img/messi.jpg");
+    });
+});
+$(document).ready(function(){
+    $("#img111").click(function(){
+$("#imgg3").attr("src","img/neymar.jpg");
+    });
+});
+$(document).ready(function(){
+    $("#img222").click(function(){
+$("#imgg3").attr("src","img/asensio.jpg");
+    });
+});
+
+$(document).ready(function(){
+    $("#img44").click(function(){
+$("#imgg2").attr("src","img/kdb.jpg");
+    });
+});
+
+$(document).ready(function(){
+    $("#img33").click(function(){
+$("#imgg2").attr("src","img/bale.jpg");
+    });
+});
+$(document).ready(function(){
+    $("#img11").click(function(){
+$("#imgg2").attr("src","img/kroos.jpg");
+    });
+});
+$(document).ready(function(){
+    $("#img22").click(function(){
+$("#imgg2").attr("src","img/pogba.jpg");
+    });
+});
